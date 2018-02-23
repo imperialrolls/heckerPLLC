5c40c509714cfc630ec733355d009094719eff18
laurentbaig <ljb0904@gmail.com>
2018-02-23 12:29:32 -0700

    changed animation to css animation. it's a little better

--- js/hecker.js
+++ js/hecker.js
@@ -9,6 +9,12 @@ $(document).ready(function() {
     $('.smooth-scroller').click(function() {
         // prevent the default action of the click
         event.preventDefault();
+
+        // if no hash defined, don't do anything
+        if (this.hash == "")
+            return;
+
+        /*
         if (this.hash !== "") {
             let hash = this.hash;
             $('html, body').animate({
@@ -17,6 +23,37 @@ $(document).ready(function() {
                 window.location.hash = hash;
             });
         }
+        */
+        function getPageScroll() {
+            let yScroll;
+
+            if (window.pageYOffset) {
+                yScroll = window.pageYOffset;
+            }else if (document.documentElement && document.documentElement.scrollTop) {
+                yScroll = document.documentElement.scrollTop;
+            }else if (document.body) {
+                yScroll = document.body.scrollTop;
+            }
+            return yScroll;
+        }
+
+        let targetOffset = document.getElementById(this.hash.substr(1)).offsetTop;
+        let currentPosition = getPageScroll();
+        let delta = targetOffset - currentPosition;
+        let xform = "translate(0, -"+delta+"px)";
+        let animateTime = 1500;
+        let body = document.body;
+
+        body.style.WebkitTransform = xform;
+        body.style.MozTransform = xform;
+        body.style.transform = xform;
+        body.classList.add('scroll-transition');
+
+        window.setTimeout(function () {
+            window.scrollTo(0, targetOffset);
+            body.classList.remove('scroll-transition');
+            body.style.cssText = "";
+        }, animateTime);
     });
 });
 
