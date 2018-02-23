a50392e7544a85630db8ebc524b6ef883a7028a8
laurentbaig <ljb0904@gmail.com>
2018-02-23 12:04:03 -0700

    added smooth scroll

--- js/hecker.js
+++ js/hecker.js
@@ -3,6 +3,20 @@ $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
 
-$(document).ready(function(){
-  $('.parallax').parallax();
+$(document).ready(function() {
+    $('.parallax').parallax();
+
+    $('.smooth-scroller').click(function() {
+        // prevent the default action of the click
+        event.preventDefault();
+        if (this.hash !== "") {
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $("#section2").offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
+
